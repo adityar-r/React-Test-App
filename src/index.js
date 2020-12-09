68681003cd7bf5e4445ca20dee988c1b9d102eb1
adityar <mailsofadityar@gmail.com>
2020-12-09 14:35:31 +0530

    add: redirect / to /home

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter,Redirect } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Redirect to={"/home"}/>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
